c67333725a796ce3fb167a9e535b279440f0e36d
lailaaria <laila.isaac@patelhs.org>
2020-11-13 14:00:46 +0000

    I added a string array for the unknown students

--- Main.java
+++ Main.java
@@ -2,5 +2,8 @@ class Main {
   public static void main(String[] args) {
     
     int[] seats = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+
+    String[] students = {"student 1", "student 2", "student 3", "student 4", "student 5", "student 6", "student 7", "student 8", "student 9", "student 10", "student 11", "student 12", "student 13", "student 14", "student 15"};
+
   }
 }
