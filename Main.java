10af794e8e1879f2c00858d1642c618b34182398
lailaaria <laila.isaac@patelhs.org>
2020-11-16 19:00:44 +0000

    I changed my string array

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ class Main {
     
     int[] seats = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
 
-    String[] students = {"student 1", "student 2", "student 3", "student 4", "student 5", "student 6", "student 7", "student 8", "student 9", "student 10", "student 11", "student 12", "student 13", "student 14", "student 15"};
+    String[] students = new String[15];
 
     for(int i = 0; i < students.length; i++){
       System.out.println(students[i]);
@@ -17,5 +17,9 @@ class Main {
     String name = scan.next();
 
     students[0] = name; 
+
+    while(int i = 0){
+      System.out.println(students + " sits in seat #" + seats);
+    }
   }
 }
