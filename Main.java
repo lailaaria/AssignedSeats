aacb3c6a677079fc1b3927ed53a4e6779f05b698
lailaaria <laila.isaac@patelhs.org>
2020-11-18 04:40:10 +0000

    I added the final while loops and print statements.

--- Main.java
+++ Main.java
@@ -7,19 +7,30 @@ class Main {
 
     String[] students = new String[15];
 
+    Scanner scan = new Scanner(System.in);
+
     for(int i = 0; i < students.length; i++){
-      System.out.println(students[i]);
-    }  
+      System.out.println("What is your first name?");
+    students[i] = scan.next();
 
-    Scanner scan = new Scanner(System.in);   
+    } 
 
-    System.out.println("What is your first name?");
-    String name = scan.next();
-
-    students[0] = name; 
-
-    while(int i = 0){
-      System.out.println(students + " sits in seat #" + seats);
+    int seats = 0;
+    while (seats <= 15)
+    {
+      System.out.println(seats + " ");
+      seats++;
     }
+
+    String students = 0;
+    while (students <= 15)
+    {
+      System.out.println(students + " ");
+      students++;
+    }
+
+    System.out.println(students[seats] + ", your seat is number " + seats);
+
+   
   }
 }
